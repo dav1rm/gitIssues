0b6b62b67ccf2b0d6a782deb322713c33ebe74e4
Davi <davirodrigues8@gmail.com>
2019-03-16 00:35:31 -0300

    Adiciona filtros a issues

--- src/pages/Issues/index.js
+++ src/pages/Issues/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  View, FlatList, ActivityIndicator, Text,
+  View, FlatList, ActivityIndicator, Text, TouchableOpacity,
 } from 'react-native';
 
 import api from '~/services/api';
@@ -25,19 +25,27 @@ export default class Issues extends Component {
     loading: false,
     refreshing: false,
     error: '',
+    state: 'all',
   };
 
   componentDidMount() {
     this.loadIssues();
   }
 
+  toFilter = async (state = 'all') => {
+    await this.setState({ state });
+
+    await this.loadIssues();
+  };
+
   loadIssues = async () => {
     this.setState({ refreshing: true, loading: true });
+    const { state } = this.state;
     const { navigation } = this.props;
-    const { id } = navigation.getParam('repository');
+    const { name, organization } = navigation.getParam('repository');
 
     try {
-      const { data } = await api.get(`/repositories/${id}/issues`);
+      const { data } = await api.get(`/repos/${organization}/${name}/issues?state=${state}`);
 
       this.setState({
         data,
@@ -66,10 +74,27 @@ export default class Issues extends Component {
   };
 
   render() {
-    const { loading, error } = this.state;
+    const { loading, error, state } = this.state;
     return (
       <View style={styles.container}>
         {!!error && <Text style={styles.error}>{error}</Text>}
+        <View style={styles.tabs}>
+          <TouchableOpacity style={styles.button} onPress={() => this.toFilter('all')}>
+            <Text style={[styles.textButton, state === 'all' ? styles.activedButton : '']}>
+              Todas
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => this.toFilter('open')}>
+            <Text style={[styles.textButton, state === 'open' ? styles.activedButton : '']}>
+              Abertas
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => this.toFilter('closed')}>
+            <Text style={[styles.textButton, state === 'closed' ? styles.activedButton : '']}>
+              Fachadas
+            </Text>
+          </TouchableOpacity>
+        </View>
         {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
       </View>
     );
--- src/pages/Issues/styles.js
+++ src/pages/Issues/styles.js
@@ -16,6 +16,30 @@ const styles = StyleSheet.create({
     marginHorizontal: metrics.baseMargin * 2,
     paddingTop: metrics.basePadding,
   },
+
+  tabs: {
+    height: 42,
+    flexDirection: 'row',
+    marginHorizontal: metrics.baseMargin * 2,
+    marginTop: metrics.baseMargin * 2,
+  },
+
+  button: {
+    flex: 1,
+    backgroundColor: colors.light,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+
+  textButton: {
+    textAlign: 'center',
+    color: colors.dark,
+  },
+
+  activedButton: {
+    fontWeight: 'bold',
+    color: colors.dark,
+  },
 });
 
 export default styles;
